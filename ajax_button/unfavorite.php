0635eda67f0517a1fa4b652ebd2af47aa79b79de
Olexandr Plakhotniuk <plaxotpos@gmail.com>
2019-02-15 12:01:53 +0200

    ajax queries

---
+++ ajax_button/unfavorite.php
@@ -0,0 +1,43 @@
+<?php
+  // You can simulate a slow server with sleep
+  // sleep(2);
+
+  session_start();
+
+  if(!isset($_SESSION['favorites'])) {
+    $_SESSION['favorites'] = [];
+  }
+
+  //del single element from an array
+ function array_remove($element, $array){
+    $index = array_search($element, $array);
+    array_splice($array, $index, 1);
+    return $array;
+ }
+
+  function is_ajax_request() {
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+      $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+  }
+
+  if(!is_ajax_request()) { exit; }
+
+  // extract $id
+  $raw_id = isset($_POST['id']) ? $_POST['id'] : '';
+
+  if(preg_match("/blog-post-(\d+)/", $raw_id, $matches)){
+    $id = $matches[1];
+
+	  // store in $_SESSION['favorites']
+      if(in_array($id, $_SESSION['favorites'])){
+		  $_SESSION['favorites'] = array_remove($id, $_SESSION['favorites']);
+      }
+
+      echo 'true';
+	  // return true/false
+  } else {
+
+    echo 'false';
+  }
+
+?>
